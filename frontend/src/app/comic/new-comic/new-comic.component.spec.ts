a01c258af808f5a88d8f5def29fdf0aa448f0e69
Donrwalsh <donrwalsh@gmail.com>
2023-11-15 20:27:47 -0600

    Fix failing unit test

--- frontend/src/app/comic/new-comic/new-comic.component.spec.ts
+++ frontend/src/app/comic/new-comic/new-comic.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewComicComponent } from './new-comic.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('NewComicComponent', () => {
   let component: NewComicComponent;
@@ -8,7 +10,8 @@ describe('NewComicComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NewComicComponent]
+      declarations: [NewComicComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
     });
     fixture = TestBed.createComponent(NewComicComponent);
     component = fixture.componentInstance;
