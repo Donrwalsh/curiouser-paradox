7aa39138139a449da46ace3e4b890fec35053d53
Donrwalsh <donrwalsh@gmail.com>
2023-12-04 14:27:59 -0600

    Fix failing unit tests

--- frontend/src/app/comic/comic-input/comic-input.component.spec.ts
+++ frontend/src/app/comic/comic-input/comic-input.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ComicInputComponent } from './comic-input.component';
+import { FormControl } from '@angular/forms';
 
 describe('ComicInputComponent', () => {
   let component: ComicInputComponent;
@@ -8,10 +9,12 @@ describe('ComicInputComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ComicInputComponent]
+      declarations: [ComicInputComponent],
     });
     fixture = TestBed.createComponent(ComicInputComponent);
     component = fixture.componentInstance;
+    component.rawFieldName = 'test';
+    component.control = new FormControl('test');
     fixture.detectChanges();
   });
 
