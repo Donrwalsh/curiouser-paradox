9d5fcd823c48c1e0591b1b8a0ca3dca7a73c7ffc
Donrwalsh <donrwalsh@gmail.com>
2023-12-14 17:58:09 -0600

    Update frontend prod environment config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiHost: 'http://134.209.160.207:3333',
-  selfHost: 'http://134.209.160.207:8080',
+  apiHost: 'http://curiouserparadox.info:3333',
+  selfHost: 'http://curiouserparadox.info',
 };
